13718fcdaf0bc9cf4ddf0583255207dd68841ad5
sefanja <sefanja_severin@hotmail.com>
2025-12-15 16:12:13 +0100

    search

--- docs/_includes/lunr/custom-index.js
+++
@@ -1,6 +0,0 @@
-// Verwijder de paginatitel uit de content die wordt geïndexeerd om dubbele resultaten te voorkomen.
-// We gebruiken .replace() omdat dit robuuster is dan .startsWith().
-
-const title = docs[i].title.trim();
-const content = docs[i].content.trim();
-docs[i].content = content.replace(title, '').trim();
